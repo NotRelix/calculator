d297b3e8164bd57fcd2d8a62400ba1b77fe68a3c
Reece Lim <lim.reecesergei@gmail.com>
2023-05-16 12:08:23 +0800

    Add basic calculator functionality

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const number = document.querySelectorAll('.number');
 const operation = document.querySelectorAll('.op');
-const display = document.querySelector('.display');
+const displayCurr = document.querySelector('.display-current');
+const displayOld = document.querySelector('.display-old');
+const equal = document.querySelector('.equal');
+
+let first = "";
+let sign = "";
+let second = "";
+
+let shouldResetScreen = false;
+
+equal.addEventListener('click', evaluate);
 
 number.forEach(num => {
     num.addEventListener('click', displayValue);
 });
 
+operation.forEach(op => {
+    op.addEventListener('click', setOperation);
+});
+
 function displayValue(e) {
-    display.textContent += e.target.value;
+    if (shouldResetScreen) resetScreen();
+    displayCurr.textContent += e.target.value;
+}
+
+function resetScreen() {
+    displayCurr.textContent = "";
+    shouldResetScreen = false;
+}
+
+function setOperation(e) {
+    first = displayCurr.textContent;
+    sign = e.target.value;
+    displayOld.textContent = `${first} ${sign}`;
+    shouldResetScreen = true;
+}
+
+function evaluate() {
+    second = displayCurr.textContent;
+    displayCurr.textContent = operate(+first, sign, +second);
+    displayOld.textContent = `${first} ${sign} ${second} =`;
 }
 
 function add(first, second) {
